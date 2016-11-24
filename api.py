4ad43675f904163fca399687aebbe95745be040c
DESKTOP-VQM44HG\Courage <ckprofmyk@gmail.com>
2016-11-24 15:50:37 +0200

    first commit with project skeleton

---
+++ api.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from  flask_restful import Api
+from Users import UserbyName,User,Userlist
+
+app=Flask(__name__)
+api=Api(app)
+api.add_resource(User, '/users/', endpoint="users")
+api.add_resource(UserbyName,'/users/<string:name>',endpoint="users/name")
+api.add_resource(Userlist,'/users/',endpoint="users/userlist")
+
+
+
+if __name__=="__main__":
+    app.run()
