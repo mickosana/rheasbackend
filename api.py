faa7497a570f840ba623bd8af404ae735927bb37
micthaworm <ckprofmyk@gmail.com>
2017-01-07 15:56:15 +0200

    addition of endpoints

--- api.py
+++ api.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from  flask_restful import Api
-from Users import UserbyName,User,Userlist
+from usersUtil import UserlistResource,UserResource
+from membersUtil import memberResource,memberlistResource
+from branchUtil import branchResource,BranchlistResource
+from expensesUtil import ExpenselistResource
+from incomeUtil import incomeResource
 from Models import db
+
 
 app=Flask(__name__)
 api=Api(app)
-api.add_resource(User, '/users/', endpoint="users")
-api.add_resource(UserbyName,'/users/<string:name>',endpoint="users/name")
-api.add_resource(Userlist,'/users/',endpoint="users/userlist")
-
+api.add_resource(UserlistResource, '/users/all')#/users
+api.add_resource(UserResource,'/users/<string:name>')#users/name
+api.add_resource(memberlistResource,'/members/all')
+api.add_resource(memberResource,'/members/<string:id>')
+api.add_resource(BranchlistResource,'/branches/all')
+api.add_resource(branchResource,'/branches/<string:branchname>')
+api.add_resource(ExpenselistResource,'/expenses')
+api.add_resource(incomeResource,'/incomes')
 
 
 if __name__=="__main__":
